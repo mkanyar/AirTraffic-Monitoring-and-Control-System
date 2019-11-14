23afb69352daf5d60fb58cb0c0bf8dd23207bffe
Mike <m_kanyar@encs.concordia.ca>
2019-11-13 19:00:45 -0500

    communication

--- comm.cpp
+++ comm.cpp
@@ -1,7 +1,33 @@
 #include "comm.h"
+#include <string>
+    string comm:: getID(){return this->ID;};
+    float comm::getX(){return this->x;}
+   	float comm::getY(){return y;};
+   	float comm::getZ(){return z;};
+   	float comm::getSpeedX(){return x;};
+   	float comm::getSpeedY(){return y;};
+   	float comm::getSpeedZ(){return z;};
+   	float comm::getEntryTime(){return entry_time;};
+   	void comm::setchangeSpeedX(float x)
+   	{
+   	speed_x=x;
+   	}
+   	void comm::setchangeSpeedY(float y)
+   	{
 
+   	 speed_y=y;
+
+   	}
 comm::comm()
 {
+	this->ID="";
+		this->x=0;
+		this->y=0;
+		this->z=0;
+		this->speed_x=0;
+		this->speed_y=0;
+		this->speed_z=0;
+
 }
 
 comm::~comm()
--- comm.h
+++ comm.h
@@ -1,12 +1,32 @@
 #ifndef COMM_H_
 #define COMM_H_
+#include <iostream>
+#include <string>
 
+using namespace std;
 class comm
-{
+{private:
+    string ID;
+	float speed_x, speed_y, speed_z, x, y, z, entry_time,changeSpeedX,changeSpeedY;
+
 public:
 	comm();
-	virtual ~comm();
-	
+   ~comm();
+  comm(string,float,float,float,float,float,float,float);
+
+    string getID();
+    float getX();
+   	float getY();
+   	float getZ();
+   	float getSpeedX();
+   	float getSpeedY();
+   	float getSpeedZ();
+   	float getEntryTime();
+   	void setchangeSpeedX(float);
+   	void setchangeSpeedY(float);
+
+
+
 	//reporting aircraft leaving airspace (sporadic)
 };
 
